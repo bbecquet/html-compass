09104f262954db37d494ad0d70b856a851a81a38
bbecquet <benjamin.becquet@gmail.com>
2022-10-02 17:16:27 +0200

    Add (useless) fetch listener in SW for installability

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const PRECACHE = 'precache-v1'
+const RUNTIME = 'runtime'
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
@@ -21,7 +22,7 @@ self.addEventListener('install', event => {
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE]
+  const currentCaches = [PRECACHE, RUNTIME]
   event.waitUntil(
     caches
       .keys()
@@ -37,27 +38,26 @@ self.addEventListener('activate', event => {
   )
 })
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-// self.addEventListener('fetch', event => {
-//   // Skip cross-origin requests, like those for Google Analytics.
-//   if (event.request.url.startsWith(self.location.origin)) {
-//     event.respondWith(
-//       caches.match(event.request).then(cachedResponse => {
-//         if (cachedResponse) {
-//           return cachedResponse
-//         }
+// No actual need for a runtime cache as the page is self-sufficient,
+// but Chrome doesn't consider the app installable without it…
+self.addEventListener('fetch', event => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse
+        }
 
-//         return caches.open(RUNTIME).then(cache => {
-//           return fetch(event.request).then(response => {
-//             // Put a copy of the response in the runtime cache.
-//             return cache.put(event.request, response.clone()).then(() => {
-//               return response
-//             })
-//           })
-//         })
-//       })
-//     )
-//   }
-// })
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response
+            })
+          })
+        })
+      })
+    )
+  }
+})
